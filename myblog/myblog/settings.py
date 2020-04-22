65443235413d42d3a5cf6a58c06bc47a0beafe3e
leords <leo_ro.drigues@hotmail.com>
2020-04-22 08:44:53 -0300

    Settings

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -18,11 +18,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g9-9o1&g3s-u=u_h1zmtdth+g-rg+2+_%1os9(xt^i4f5wpr#n'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
